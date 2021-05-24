6808f9b29a73e419f90d9af285fe278ac6d8c523
Alex Merced <alex@alexmerced.dev>
2021-05-24 19:42:11 -0400

    removed solutions

--- w10d04/5_afternoon_lab/pets_solution/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :birds
-  resources :cats
-  resources :dogs
-  resources :turtles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
